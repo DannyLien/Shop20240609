4515afb1fc76d0aadaa227194428069250fae891
DannyLien <danny.lien1109@gmail.com>
2024-06-09 23:15:50 +0800

    6-2, RecyclerView, log;

--- app/src/main/java/com/tom/shop20240609/MainActivity.kt
+++ app/src/main/java/com/tom/shop20240609/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.tom.shop20240609
 
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -9,10 +11,29 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.tom.shop20240609.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    val TAG = MainActivity::class.java.simpleName
+    val functions = listOf<String>(
+        "Camera",
+        "Invite friend",
+        "Parking",
+        "Download coupons",
+        "News",
+        "Maps",
+        "A",
+        "B",
+        "Love You",
+        "Good Job",
+        "Free"
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +45,42 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+
+        //recycler
+        binding.contentMain.recycler.layoutManager = LinearLayoutManager(this)
+        binding.contentMain.recycler.setHasFixedSize(false)
+        binding.contentMain.recycler.adapter = FunctionAdapter()
+
+    }
+
+    inner class FunctionAdapter : RecyclerView.Adapter<FunctionHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FunctionHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.row_function, parent, false)
+            val holder = FunctionHolder(view)
+            return holder
+        }
+
+        override fun getItemCount(): Int {
+            return functions.size
+        }
+
+        override fun onBindViewHolder(holder: FunctionHolder, position: Int) {
+            holder.nameText.text = functions.get(position)
+            holder.itemView.setOnClickListener { view ->
+                functionClicked(holder, position)
+            }
+        }
+    }
+
+    private fun functionClicked(holder: FunctionHolder, position: Int) {
+        Log.d(TAG, "functionClicked: $position")
+       
+    }
+
+
+    class FunctionHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var nameText: TextView = view.findViewById(R.id.name)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
