935f0414a9b860f30166e3c9ef8538c0bae25777
DannyLien <danny.lien1109@gmail.com>
2024-06-17 11:02:56 +0800

    7-3, 使用 Json To Kotlin 協助建立類別, 使用 Gson解析 json;

--- app/src/main/java/com/tom/shop20240609/ParkingActivity.kt
+++ app/src/main/java/com/tom/shop20240609/ParkingActivity.kt
@@ -3,11 +3,14 @@ package com.tom.shop20240609
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.JsonReader
 import android.util.Log
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView.LayoutManager
+import com.google.gson.Gson
 import com.tom.shop20240609.databinding.ActivityParkingBinding
 import java.net.URL
+
 
 class ParkingActivity : AppCompatActivity() {
     private lateinit var binding: ActivityParkingBinding
@@ -17,8 +20,7 @@ class ParkingActivity : AppCompatActivity() {
         binding = ActivityParkingBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val parking =
-            "https://api.jsonserve.com/TyZaEe"
+        val parking = "https://api.jsonserve.com/TyZaEe"
         ParkingTask().execute(parking)
 
     }
@@ -35,9 +37,56 @@ class ParkingActivity : AppCompatActivity() {
         override fun onPostExecute(result: String?) {
             super.onPostExecute(result)
             Toast.makeText(this@ParkingActivity, "Got it", Toast.LENGTH_LONG).show()
+            parseGson(result)
             binding.info.text = result
 
         }
     }
 
+    private fun parseGson(json: String?) {
+        val parking = Gson().fromJson<Parking>(json, Parking::class.java)
+        val jsonSize = parking.parkingLots.size
+        parking.parkingLots.forEach {
+            Log.d(TAG, "parseGson: = ${it.areaId},${it.areaName},${it.parkName},${it.totalSpace}")
+        }
+    }
+
+    data class Parking(
+        val parkingLots: List<ParkingLot>
+    )
+
+    data class ParkingLot(
+        val address: String,
+        val areaId: String,
+        val areaName: String,
+        val introduction: String,
+        val parkId: String,
+        val parkName: String,
+        val payGuide: String,
+        val surplusSpace: String,
+        val totalSpace: Int,
+        val wgsX: Double,
+        val wgsY: Double
+    )
+
 }
+
+/*
+{
+    "parkingLots": [
+        {
+            "areaId": "3",
+            "areaName": "八德區",
+            "parkName": "大湳公有停車場",
+            "totalSpace": 207,
+            "surplusSpace": "112",
+            "payGuide": "小型車-臨停:30/時，小型車-月租:3600/月",
+            "introduction": "八德區公所管轄之公有停車場",
+            "address": "廣福路42號(廣福段258地號)",
+            "wgsX": 24.959,
+            "wgsY": 121.2985,
+            "parkId": "P-BD-001"
+        }
+    ]
+}
+*/
